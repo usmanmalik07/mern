83def29fe791fe1b8f6bd83d0c7cbe28a06235ca
Usman Malik <usmanmalikk2004@gmail.com>
2024-03-13 09:58:44 +0500

    13march

--- 13mar/index.js
+++ 13mar/index.js
@@ -1,9 +1,21 @@
-var fs = require('fs');
-fs.readFile(__filename,(err, data) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(data.toString());
-    }
+// var fs = require('fs');
+// fs.readFile(__filename,(err, data) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(data.toString());
+//     }
 
-})
+// })
+var fs = require('fs');
+var stream = fs.createReadStream('foo.txt');
+stream.on("data",function(data){
+    var chunk = data.toString();
+    process.stdout.write(chunk);
+
+});
+stream.on("end",function(){
+    console.log();});
+stream.on('error',function(erro){
+    console.log(error.message);
+});    
